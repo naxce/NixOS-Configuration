90dbcd25af83ea811d09a4170f445003770d804e
naxce <naxcehelltea@gmail.com>
2026-05-05 22:59:27 +0200

    Update wto, 5 maj 2026, 22:59:27 CEST

--- home.nix
+++ home.nix
@@ -19,9 +19,9 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      nixos = "cd ~/dotfiles && git add -A && git commit -m \"Update $(date)\" && git push && sudo nixos-rebuild switch --flake .#naxce";
+      nixos = "cd ~/dotfiles && git add . && git reset nixos/hardware-configuration.nix && (git commit -m \"Update $(date)\" || true) && git push origin main --force && git add -f -N nixos/hardware-configuration.nix && sudo nixos-rebuild switch --flake .#naxce";
 
-      onlybuild = "cd ~/dotfiles && sudo nixos-rebuild switch --flake .#naxce";
+      onlybuild = "cd ~/dotfiles && git add -f -N nixos/hardware-configuration.nix && sudo nixos-rebuild switch --flake .#naxce";
     };
   };
 
