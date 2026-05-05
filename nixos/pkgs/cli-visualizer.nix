d6a5c3d26f9b4cd1bae6918a1b750aae7af14854
naxce <naxcehelltea@gmail.com>
2026-05-06 01:39:49 +0200

    Update śro, 6 maj 2026, 01:39:49 CEST

--- nixos/pkgs/cli-visualizer.nix
+++ nixos/pkgs/cli-visualizer.nix
@@ -10,7 +10,7 @@ pkgs.stdenv.mkDerivation {
     owner = "PosixAlchemist";
     repo = "cli-visualizer";
     rev = "v1.6";
-    sha256 = pkgs.lib.fakeSha256;
+    sha256 = "DurKc9g9SQT7AJSYoJsM5eWphPZLvkJ7Gg4KsXIdQl4=";
   };
 
   nativeBuildInputs = with pkgs; [
