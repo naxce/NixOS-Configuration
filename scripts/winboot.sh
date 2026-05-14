1630fb2495d760b0e553d02f057c94665bff86a2
naxce <naxcehelltea@gmail.com>
2026-05-14 20:36:34 +0200

    Update Thu May 14 20:36:34 CEST 2026

--- scripts/winboot.sh
+++ scripts/winboot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 exec 2> /home/naxce/winboot_error.log
 
-printf 'timeout -1\ndefault_selection "Windows 11"\n' | sudo tee /boot/EFI/refind/manual_boot.conf && sudo systemctl reboot
+printf 'timeout -1\ndefault_selection "Windows 11"\n' | sudo /run/current-system/sw/bin/tee /boot/EFI/refind/manual_boot.conf && sudo /run/current-system/sw/bin/systemctl reboot
